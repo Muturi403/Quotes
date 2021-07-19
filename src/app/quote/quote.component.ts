6fc3284e1a8f7c2edd0f3b9b1951c06b366f756f
Muturi403 <faith.muturi@student.moringaschool.com>
2021-07-19 20:09:26 +0300

    styled the upvote, downvote and delete button

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -13,7 +13,7 @@ export class QuoteComponent implements OnInit {
     new Quote('Grief does not change you, Hazel. It reveals you.', 'Faith Muturi', 'John Green', new Date(2021, 6, 16), 0, 0),
     new Quote('It is only after you’ve stepped outside your comfort zone that you begin to change, grow, and transform.', 'Faith Muturi', 'Roy T. Bennett', new Date(2021, 6, 16), 0, 0),
     new Quote('Yesterday I was clever, so I wanted to change the world. Today I am wise, so I am changing myself.', 'Faith Muturi', 'Rumi', new Date(2021, 6, 16), 0, 0),
-    new Quote('You can not stop the future You can not rewind the past.The only way to learn the secret is to press play.', 'Faith Muturi', 'Jay Asher', new Date(2021, 6, 16), 0, 0)
+    new Quote('You can not stop the future, You can not rewind the past.The only way to learn the secret is to press play.', 'Faith Muturi', 'Jay Asher', new Date(2021, 6, 16), 0, 0)
   ]; 
   get sortQuotes() {
     return this.quotes.sort((a, b) => {
